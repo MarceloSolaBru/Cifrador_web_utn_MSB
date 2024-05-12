a2b880739fc948c101f53b9a576a3ffe8fe59c4c
Marcelo Sola Bru <marcelosola2001@gmail.com>
2024-05-12 17:49:32 -0300

    .

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from app import create_app
+from app import create_app, db
+from flask import Flask
+from app.routes.index import index
+from app.models import Text, TextHistory, User, Role, UserData
 
 app = create_app()
 
-#https://flask.palletsprojects.com/en/3.0.x/appcontext/
+
+# register the blueprint
+app.register_blueprint(index)
+
+with app.app_context():
+    # Create tables
+    db.create_all()
+
+# https://flask.palletsprojects.com/en/3.0.x/appcontext/
 app.app_context().push()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Server Startup
     Ref: https://flask.palletsprojects.com/en/3.0.x/api/#flask.Flask.run
     Ref: Book Flask Web Development Page 9
     """
     app.run(host="0.0.0.0", port=5000)
-    
