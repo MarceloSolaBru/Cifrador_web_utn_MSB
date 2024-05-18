9be4ceb5314b33bb0213f23a933795c9f6d503d9
Marcelo Sola Bru <marcelosola2001@gmail.com>
2024-05-18 20:09:25 -0300

    add profile

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,16 +1,7 @@
 # Importa las clases User, Role y UserData desde sus respectivos archivos en el mismo directorio
-from .user import (
-    User,
-)  # Importa la clase User desde el archivo user.py en el mismo directorio
-from .role import (
-    Role,
-)  # Importa la clase Role desde el archivo role.py en el mismo directorio
-from .user_data import (
-    UserData,
-)  # Importa la clase UserData desde el archivo user_data.py en el mismo directorio
-from .text import (
-    Text,
-)  # Importa la clase Text desde el archivo text.py en el mismo directorio
-from .text_history import (
-    TextHistory,
-)  # Importa la clase TextHistory desde el archivo text_history.py en el mismo directorio
+from .user import (User,)  # Importa la clase User desde el archivo user.py en el mismo directorio
+from .role import (Role,)  # Importa la clase Role desde el archivo role.py en el mismo directorio
+from .user_data import (UserData,)  # Importa la clase UserData desde el archivo user_data.py en el mismo directorio
+from .text import (Text,)  # Importa la clase Text desde el archivo text.py en el mismo directorio
+from .text_history import (TextHistory,)  # Importa la clase TextHistory desde el archivo text_history.py en el mismo directorio
+from .profile import Profile
--- app/models/user_data.py
+++ app/models/user_data.py
@@ -7,30 +7,22 @@ from app import db
 
 # Define una clase llamada UserData utilizando el decorador dataclass
 @dataclass(init=False, repr=True, eq=True)
-class UserData(
-    db.Model
-):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
+class UserData(db.Model):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
     __tablename__ = "users_data"  # Nombre de la tabla en la base de datos
-    id: int = db.Column(
-        db.Integer, primary_key=True, autoincrement=True
-    )  # Columna de clave primaria
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Columna de clave primaria
     firstname: str = db.Column(db.String(80), nullable=False)
     lastname: str = db.Column(db.String(80), nullable=False)
-    phone: str = db.Column(
-        db.String(120), nullable=False
-    )  # Columna para el número de teléfono del usuario
-    address: str = db.Column(
-        db.String(120), nullable=False
-    )  # Columna para la dirección del usuario
-    city: str = db.Column(
-        db.String(120), nullable=False
-    )  # Columna para la ciudad del usuario
-    country: str = db.Column(
-        db.String(120), nullable=False
-    )  # Columna para el país del usuario
+    phone: str = db.Column(db.String(120), nullable=False)  # Columna para el número de teléfono del usuario
+    address: str = db.Column(db.String(120), nullable=False)  # Columna para la dirección del usuario
+    city: str = db.Column(db.String(120), nullable=False)  # Columna para la ciudad del usuario
+    country: str = db.Column(db.String(120), nullable=False)  # Columna para el país del usuario
 
     # Columna de clave externa para establecer la relación con la tabla 'users' (usuarios)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
 
     # Relación con la tabla 'User' (usuarios), establecida a través de la columna 'user_id'
     user = db.relationship("User", back_populates="data", uselist=False)
+    
+    #Relacion Muchos a Uno bidireccional con Profile
+    profile_id = db.Column('profile_id', db.Integer, db.ForeignKey('profiles.id'))
+    profile = db.relationship("Profile", back_populates='data')
