ee231a4a5c118c3f4159c632aa3b8fa4533e0b87
Marcelo Sola Bru <marcelosola2001@gmail.com>
2024-05-21 22:15:15 -0300

    text legible

--- app/models/text.py
+++ app/models/text.py
@@ -1,31 +1,15 @@
-# Importa el decorador dataclass desde el módulo dataclasses
 from dataclasses import dataclass
-
-# Importa la instancia db desde el módulo app, que parece ser un objeto de SQLAlchemy
 from app import db
 from typing import List
-
 from cryptography.fernet import Fernet
 
-
-# Definimos una clase llamada Text utilizando el decorador dataclass
 @dataclass(init=False, repr=True, eq=True)
-class Text(
-    db.Model
-):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
+class Text(db.Model):  # Hereda de db.Model, lo que indica que es un modelo de base de datos
     __tablename__ = "texts"  # Nombre de la tabla en la base de datos
-    id: int = db.Column(
-        db.Integer, primary_key=True, autoincrement=True
-    )  # Columna de clave primaria
-    content: str = db.Column(
-        db.String(120), nullable=False
-    )  # Columna para el texto del usuario
-    length: int = db.Column(
-        db.Integer, nullable=False
-    )  # columna que indica el tamaño del texto
-    language: str = db.Column(
-        db.String(120), nullable=False
-    )  # columna que indica el idioma del texto
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Columna de clave primaria
+    content: str = db.Column(db.String(120), nullable=False)  # Columna para el texto del usuario
+    length: int = db.Column(db.Integer, nullable=False)  
+    language: str = db.Column(db.String(120), nullable=False)  
     # Define la relación con TextHistory
     histories = db.relationship("TextHistory", backref="text", lazy=True)
     user_id: int = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
