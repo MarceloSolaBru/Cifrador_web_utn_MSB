f781cd8316bc2a677da24936ce0021b9a63f3b4e
Marcelo Sola Bru <marcelosola2001@gmail.com>
2024-09-02 17:19:57 -0300

    response_schema and msg

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,8 @@
 import unittest
 from flask import current_app
 from app import create_app
+from app.mapping.response_schema import ResponseSchema
+from app.services.response_message import ResponseBuilder
 
 class AppTestCase(unittest.TestCase):
     # Método de configuración que se ejecuta antes de cada prueba
@@ -22,11 +24,17 @@ class AppTestCase(unittest.TestCase):
         self.assertIsNotNone(current_app)
 
     def test_index(self):
+        message = ResponseBuilder().add_message("Bienvenidos").add_status_code(200).add_data({'title': 'API Auth'}).build()
         client = self.app.test_client(use_cookies=True)
+        responseSchema = ResponseSchema()
         #TODO: La URL de la API debe cambiarse por una variable de entorno
         response = client.get('http://localhost:5000/api/v1/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'OK', response.data)
+        response = responseSchema.load(response.get_json())
+        self.assertEqual(message.message, response['message'])
+        self.assertEqual(message.status_code, response['status_code'])
+        self.assertEqual(message.data, response['data'])
+        
 
 # Ejecuta el conjunto de pruebas si el script se ejecuta directamente
 if __name__ == '__main__':
